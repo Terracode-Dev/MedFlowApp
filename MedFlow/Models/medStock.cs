946362909f7e44a9f1cd2b1993048d93cb3a642b
Nomin Sendinu <soloriderns@gmail.com>
2023-08-23 08:49:18 +0530

    added models and Migrate scripts

---
+++ MedFlow/Models/medStock.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedFlow.Models
+{
+    public class medStock
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public int stocks { get; set; }
+    }
+}
---
+++ MedFlow/Models/prescriptions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedFlow.Models
+{
+    public class Prescriptions
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string filepath { get; set; }
+
+        public int patient_id { get; set; }
+        public int payment_id { get; set; }
+        
+
+        //Realtions
+        public Patient patient { get; set; }
+        public Payments payments { get; set; }
+        
+
+    }
+}
