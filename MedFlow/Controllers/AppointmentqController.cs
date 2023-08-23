00ccc5efb5c836dd3b85a9c1c81e6fe8453505ed
theDewill <soloriderns@gmail.com>
2023-08-23 22:35:57 +0530

    fixing forms and rerendering

---
+++ MedFlow/Controllers/AppointmentqController.cs
@@ -0,0 +1,34 @@
+﻿using MedFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedFlow.Controllers
+{
+    public class AppointmentqController : Controller
+    {
+
+        private readonly ApplicationDbContext DbContext;
+        public AppointmentqController(ApplicationDbContext Context)
+        {
+            DbContext = Context;
+        }
+        public object SearchQuery(string qname)
+        {
+            var dataset = DbContext.patients.Where(patient => patient.name == qname).ToList();
+            return dataset;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Search(string name) {
+
+            //url ---> Appoinmentq/Search
+            var data = SearchQuery(name);
+            return PartialView(data);
+        }
+    }
+}
