f096456ae3627772e94898622de34988e992ad72
theDewill <soloriderns@gmail.com>
2023-08-24 11:35:54 +0530

    rewired UI initial stage

---
+++ MedFlow/Util/QueueProcess.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+namespace MedFlow.Util
+{
+    public class QueueProcess
+    {
+        private Queue<object> Que = new Queue<object>();
+        private Thread pushThread;
+        private Thread pullThread;
+
+        public QueueProcess() {
+            pushThread = new Thread(pushTHandle);
+            pullThread = new Thread(pullTHandle);
+        }
+
+        private void pushTHandle ()
+        {
+
+        }
+
+        private void pullTHandle ()
+        {
+
+        }
+        public void PushData(object data)
+        {
+            
+        }
+
+        public object PullData ()
+        {
+            return Que.Dequeue();
+        }
+
+
+    }
+}
